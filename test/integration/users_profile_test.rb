7c7f129397f5bb702395226a54667ff9d0c2f613
hodanov <1031hoda@gmail.com>
2018-08-09 00:19:56 +0000

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -13,9 +13,18 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select '.box-user-info h1', text: @user.name
     assert_select '.box-user-image img.gravatar'
     assert_match @user.microposts.count.to_s, response.body
+    assert_select '.stats #following', text: @user.following.count.to_s
+    assert_select '.stats #followers', text: @user.followers.count.to_s
     assert_select 'div.pagination', count: 1
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+  end
+
+  test "stats display" do
+    log_in_as(@user)
+    get root_path
+    assert_select '.stats #following', text: @user.following.count.to_s
+    assert_select '.stats #followers', text: @user.followers.count.to_s
   end
 end
